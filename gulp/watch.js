9afa2b155c235d6912281e26efd57a8e29626746
PBelugin <pashaigood@gmail.com>
2014-07-25 00:09:12 +0400

    initial settings

---
+++ gulp/watch.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var gulp = require('gulp');
+
+var browserSync = require('browser-sync');
+
+var $ = require('gulp-load-plugins')();
+
+gulp.task('watch', ['wiredep', 'styles', 'scripts'], function () {
+    gulp.watch('app/partials/**/*.html');
+    gulp.watch('app/styles/**/*.scss', ['styles']);
+    gulp.watch('app/scripts/**/*.js', ['scripts']);
+    gulp.watch('app/images/**/*', ['images']);
+    gulp.watch('bower.json', ['wiredep']);
+});
